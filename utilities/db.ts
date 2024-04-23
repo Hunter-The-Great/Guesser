903a27aab874fc8dca10799281dfd28098c6923b
Hunter-The-Great <benlowry9@gmail.com>
2024-04-23 15:24:03 -0500

    last ditch effort to just keep the bot online despite prisma being uncooperative

--- utilities/db.ts
+++ utilities/db.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { sentry } from "./sentry";
 
-const prisma = new PrismaClient();
+let prisma;
+try {
+    prisma = new PrismaClient();
+} catch (err) {
+    sentry.captureException(err);
+    console.log("WARNING: prisma client not generated");
+}
 
 export { prisma };
