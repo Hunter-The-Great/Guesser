1bb68e2c5f5f3851b34892fa820d097cd363b33d
Hunter-The-Great <benlowry9@gmail.com>
2024-04-23 09:57:47 -0500

    updated help menu

--- commands/general/help.ts
+++ commands/general/help.ts
@@ -20,13 +20,22 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
             iconURL: interaction.user.displayAvatarURL(),
         })
         .addFields({
-            name: "_**In progress**__",
-            value: "I'll update this menu soon.",
+            name: "__**Game**__",
+            value: "**guessing-game**: Starts the game.",
             inline: true,
         })
-        .setFooter({
-            text: "idk",
-            iconURL: process.env.AVATAR,
+        .addFields({
+            name: "__**General**__",
+            value: `**help**: Shows this menu.\n\n
+                    **feedback**: Provides feedback to the author of the bot.`,
+            inline: true,
+        })
+        .addFields({
+            name: "__**Utility**__",
+            value: `**channel**: Manages the channels used for the game.\n*(Only applicable with logging disabled)*\n\n
+                    **logging**: Toggles message logging on/off.\n*(Off by default)*\n\n
+                    **log-messages**: Logs all messages in the server.\n*(Use this sparingly, it takes a long time)*`,
+            inline: true,
         });
 
     await interaction.reply({ embeds: [helpMenu] });
